61e86f0d9bf927aaa6d1dbae72f02802c2dd67b4
Ian Channing <ian@klever.co.uk>
2014-05-28 01:05:22 +0200

    update, added in link comment

--- content/themes/aquarius-child/functions.php
+++ content/themes/aquarius-child/functions.php
@@ -1,7 +1,12 @@
 <?php
 
-// Woocommerce New Customer Admin Notification Email
+
 add_action('woocommerce_created_customer', 'icc_aquarius_child_admin_email_on_registration');
+/**
+ * Woocommerce New Customer Admin Notification Email
+ * 
+ * @link http://wordpress.org/support/topic/woocommerce-new-registration-not-sending-email-to-admin?replies=3#post-4374790
+ */
 function icc_aquarius_child_admin_email_on_registration() {
     $user_id = get_current_user_id();
     wp_new_user_notification( $user_id );
